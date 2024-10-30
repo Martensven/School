d5a0341a6adb80b501f00f589263242dc109ee4f
Martensven <martengullstrom@gmail.com>
2024-10-30 12:42:15 +0200

    Trying out JavaScript
    Added project

---
+++ JavaScript 1/App.js
@@ -0,0 +1,7 @@
+let age = 30; //Din ålder. Den går upp varje år, därför: let. Datatyp: number. 
+let name = "Mårten"; //Ditt namn. Man kan byta namn, därför: let. Datatyp: string.
+let isStudent = true; //Är du student? True eller False. Man är inte student för evigt, därför: let. Datatyp: boolean.
+let city = 'Stockholm'
+city = 'Eskilstuna'
+
+console.log ("Jag heter " + name + ", jag är " + age + "år gammal och bor i " + city + ".");
