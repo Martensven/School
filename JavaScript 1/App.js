f2c422ae6faa9c52832fbab400f89a365f597cbd
Martensven <martengullstrom@gmail.com>
2024-11-04 12:20:27 +0100

    a

--- JavaScript 1/App.js
+++ JavaScript 1/App.js
@@ -1,7 +1,55 @@
-let age = 30; //Din ålder. Den går upp varje år, därför: let. Datatyp: number. 
-let name = "Mårten"; //Ditt namn. Man kan byta namn, därför: let. Datatyp: string.
-let isStudent = true; //Är du student? True eller False. Man är inte student för evigt, därför: let. Datatyp: boolean.
-let city = 'Stockholm'
-city = 'Eskilstuna'
+// const types = ["Boolean"];
+// types [1] = "String";
+// types [2] = "Number";
+// types [3] = "Null";
+// types [4] = "Undefined";
+// types [5] = "NaN";
 
-console.log ("Jag heter " + name + ", jag är " + age + "år gammal och bor i " + city + ".");
+// let cars = ["volvo", "Saab"];
+
+// let myName = "Mårten";
+// const siffra = 2;
+
+
+
+// console.log ("Hej jag heter " + myName + "och jag är " + siffra + " År");
+// console.log ("Här är några av mina favoritdatatyper: " + types); 
+// console.log (cars); 
+
+
+
+// Code along plusPoäng
+
+function greet () {
+    let input = document.getElementById("userName").value
+    let firstName = input; 
+    document.getElementById("welcome").innerHTML = "Välkommen " + firstName + "!";
+    // console.log("Hej " + firstName);
+}
+
+
+
+
+// greet ();
+
+
+// let tal1 = Number(prompt("Ange ett tal"));
+// let tal2 = Number(prompt("Ange ett till tal"));
+// let tal3 = Number(prompt ("ange ett tal: "));
+
+// let summa = tal1 + tal2;
+// let diff = tal1 - tal2;
+// let produkt = tal1 * tal2;
+// let rundat = Math.round(tal3);
+// let decimal = tal3.toFixed(2);
+
+// console.log ("Summan är: " + summa);
+// console.log ("Det differensen med: " + diff);
+// console.log ("Produkten är: " + produkt);
+// console.log ("Avrundat tal blir: " + rundat)
+// console.log ("Avrundat tal till två decimaler: " + decimal)
+
+
+
+
+
